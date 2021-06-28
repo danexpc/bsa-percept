17926fa67ca22afbf9695dd0a73af67242efc250
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-28 08:13:18 +0300

    Implement saving to storage

--- src/main/java/bsa/java/concurrency/fs/FileSystemRepository.java
+++ src/main/java/bsa/java/concurrency/fs/FileSystemRepository.java
@@ -1,19 +1,39 @@
 package bsa.java.concurrency.fs;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 @Repository
 public class FileSystemRepository implements FileSystem {
 
-    @Value("${application.resources.static-location}")
-    private String pathToStorage;
+    private final Path pathToStorage;
+
+    @SneakyThrows
+    public FileSystemRepository(@Value("${application.resources.static-location}") String pathToStorage) {
+        this.pathToStorage = Path.of(pathToStorage);
+        if (!Files.exists(this.pathToStorage)) {
+            Files.createDirectories(this.pathToStorage);
+        }
+    }
+
 
     @Override
-    public CompletableFuture<String> saveFile(UUID id, byte[] file) {
-        return null;
+    @SneakyThrows
+    public CompletableFuture<String> saveFile(UUID name, byte[] file) {
+        return CompletableFuture.supplyAsync(() -> supplySaveToFile(name, file));
+    }
+
+    @SneakyThrows
+    private String supplySaveToFile(UUID name, byte[] file) {
+        return Files.write(Path.of(pathToStorage.toString(), name + ".jpg"),
+                file, StandardOpenOption.CREATE).toString();
     }
 }
