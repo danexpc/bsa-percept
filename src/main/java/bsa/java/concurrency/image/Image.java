ba17d68d34caa7507f47103c8afd5beed2615687
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-28 08:38:22 +0300

    Create image entity and assign it to database table

---
+++ src/main/java/bsa/java/concurrency/image/Image.java
@@ -0,0 +1,31 @@
+package bsa.java.concurrency.image;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @Column
+    private UUID id;
+
+    @Column
+    private Long hash;
+
+    @Column
+    private String path;
+}
