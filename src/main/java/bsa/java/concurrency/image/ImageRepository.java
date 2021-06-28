78060a818f30df7919fd238a53206db5b9763748
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-28 08:41:02 +0300

    Create ImageRepository which extends from JPARepository

---
+++ src/main/java/bsa/java/concurrency/image/ImageRepository.java
@@ -0,0 +1,8 @@
+package bsa.java.concurrency.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ImageRepository extends JpaRepository<Image, UUID> {
+}
