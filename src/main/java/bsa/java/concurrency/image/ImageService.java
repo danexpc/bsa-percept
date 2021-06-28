520f3dcd2130ac9070094cb31b56d0fa6f370608
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-28 08:48:02 +0300

    Save builded image to database

--- src/main/java/bsa/java/concurrency/image/ImageService.java
+++ src/main/java/bsa/java/concurrency/image/ImageService.java
@@ -1,6 +1,7 @@
 package bsa.java.concurrency.image;
 
 import bsa.java.concurrency.fs.FileSystemService;
+import bsa.java.concurrency.image.hash.DHasher;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,11 @@ public class ImageService {
     @Autowired
     private FileSystemService fsService;
 
+    @Autowired
+    private ImageRepository repository;
+
+    private final DHasher hasher = new DHasher();
+
     public void uploadImages(MultipartFile[] files) {
         Arrays.stream(files).forEach(this::uploadImage);
     }
@@ -26,6 +32,12 @@ public class ImageService {
         var bytes = file.getBytes();
         var uuid = UUID.randomUUID();
         var pathToImage = fsService.saveFile(uuid, bytes);
-        log.error(pathToImage);
+        var hash = hasher.calculateHash(bytes);
+        repository.save(
+                Image.builder()
+                        .id(uuid)
+                        .hash(hash)
+                        .path(pathToImage)
+                        .build());
     }
 }
