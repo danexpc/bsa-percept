374d1295e5c772cf60c54d2321af2773dd466180
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-29 08:56:55 +0300

    Implement DHasher from Hasher interface

--- src/main/java/bsa/java/concurrency/image/hash/DHasher.java
+++ src/main/java/bsa/java/concurrency/image/hash/DHasher.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 
-public class DHasher {
+public class DHasher implements Hasher {
 
     public long calculateHash(byte[] image) {
         try {
