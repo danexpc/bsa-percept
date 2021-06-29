ca2abc5f90e3c0d510dea16693ac070696279ecf
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-06-29 21:48:01 +0300

    Change constructor parameters

--- src/main/java/bsa/java/concurrency/exception/UnsupportedHasherException.java
+++ src/main/java/bsa/java/concurrency/exception/UnsupportedHasherException.java
@@ -1,7 +1,7 @@
 package bsa.java.concurrency.exception;
 
 public class UnsupportedHasherException extends RuntimeException {
-    public UnsupportedHasherException(String message, Throwable cause) {
-        super(message, cause);
+    public UnsupportedHasherException(String message) {
+        super(message);
     }
 }
